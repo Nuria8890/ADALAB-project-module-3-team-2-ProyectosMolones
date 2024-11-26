f94b56a3b8ff27aa7d6ab8913aca14bdcf11b73c
Crisferdi8 <fernandezcristina87@gmail.com>
2024-11-26 14:54:31 +0100

    add github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/project-promoA-module-3-team-2-ProyectosMolones/",
 })
