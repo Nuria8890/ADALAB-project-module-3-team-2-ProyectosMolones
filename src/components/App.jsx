3828afc779e1eb70babf7a82f13e28df398a4bb2
Nuria Campo <nuria.campo7@gmail.com>
2024-11-30 11:42:58 +0100

    import localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
 import { useState } from "react";
+import localStorage from "../services/localStorage";
 // import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
